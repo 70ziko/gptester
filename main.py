5e585c6dab1b783736d95f33115118501b7cd129
PatrykF <103563386+70ziko@users.noreply.github.com>
2023-11-08 22:03:53 +0100

    first argument

--- main.py
+++ main.py
@@ -2,18 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import argparse
 
 # obsługa programu poprzez argumenty przekazywane w konsoli
-parser = argparse.ArgumentParser(description='Program porównujący zainstalowane oprogramowanie z bazą danych podatności \n')
-parser.add_argument('-v','--verbose', help='Zawrzyj opisy podatności w wyniku, wypisz wszystkie wykonywane kroki', action='store_true')
-parser.add_argument('-d','--csv', type=str , help='Określ ścieżkę projektu przeznaczonego do skanowania', default=".", metavar='PATH')
+parser = argparse.ArgumentParser(description='Static Code Analysis Agent\n')
+parser.add_argument('directory', type=str, help='Path to the directory to scan')
+parser.add_argument('-v', '--verbose', help='Zawrzyj opisy podatności w wyniku, wypisz wszystkie wykonywane kroki', action='store_true')
+
+args = parser.parse_args()
+
+print(args.directory)
 
 
 args=parser.parse_args()
 
 def main():
-    if args.NVD and args.verbose:
-        print("Przeszukiwanie bazy danych podatności NVD")
+    if args.directory:
+        print("Directory: " + args.directory)
 
 if __name__ == "__main__":
     main()
